e944f7a9c38bc43fe4454d59694d092d15973d64
DavidSilady <david.silady@gmail.com>
2023-07-12 14:28:19 +0200

    fixed build errors

--- src/components/molecules/SearchResultItem.tsx
+++ src/components/molecules/SearchResultItem.tsx
@@ -1,6 +1,8 @@
 import { SearchEntityItem } from "@/models/SearchEntityItem";
 import SearchResultAttribute from "../atoms/SearchResultAttribute";
-// import { BsBuilding, BsPersonFill } from "react-icons/bs"
+import { BsBuildingFill, BsPersonFill } from "react-icons/bs"
+import Link from "next/link";
+import { Button } from "@mui/material";
 
 type Props = {
   key: string;
@@ -8,13 +10,20 @@ type Props = {
 }
 
 export default function SearchResultItem({ key, entity }: Props) {
+  const iconClasses = "mr-2"
+  const icon = entity.type === "person" ? <BsPersonFill className={iconClasses} /> : <BsBuildingFill className={iconClasses} />
   return (
-    <div key={key} className="mt-8 w-2/3">
-      {/* <h3 className="text-adversea-green text-xl"><BsBuilding />{entity.name}</h3> */}
-      <div className="text-adversea-grey">
-        <SearchResultAttribute label="AGE" value={entity.age} />
-        <SearchResultAttribute label="LOCATION" value={entity.dominantLocation} />
-        <SearchResultAttribute label="ARTICLES" value={entity.numArticles} />
+    <div key={key} className="mt-8 flex items-center">
+      <div className="w-2/3">
+        <Link href={entity.pageUrl}><h3 className="text-adversea-green text-xl flex items-center">{icon}{entity.name}</h3></Link>
+        <div className="text-adversea-grey">
+          <SearchResultAttribute label="AGE" value={entity.age} />
+          <SearchResultAttribute label="LOCATION" value={entity.dominantLocation} />
+          <SearchResultAttribute label="ARTICLES" value={entity.numArticles} />
+        </div>
+      </div>
+      <div className="ml-auto">
+        <Button variant="contained">DETAIL</Button>
       </div>
     </div>
   )
